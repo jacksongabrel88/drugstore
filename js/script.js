42404508c00810277fc379b5a10fe500d58bbe96
jacksongabriel88@gmail.com <jacksongabriel88@gmail.com>
2020-07-11 21:26:55 -0300

    teste de js para reposicionar banner

--- js/script.js
+++ js/script.js
@@ -22,3 +22,59 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     }
   });
+  
+  
+  // convém estar no onload da janela
+window.onload = function()
+{
+  var banner =  document.getElementById('banner_carousel');
+  // console.info(banner.src);
+  // primeiro tens de sacar a dimensão da janela
+  // neste caso a área visível do browser
+  var largura = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+
+  // depois é brincar com if's e colocar o banner que queiras:
+
+  if(largura>=1440)
+  {
+    banner.src = 'img/banner_drogstore_large_1440.png';
+    //console.info(banner.src);
+  } else {
+    banner.src = 'img/banner_drogstore_large_1024.png';
+    //console.info(banner.src);
+  }
+} // fim do window.onload
+
+function sizeOfThings(){
+    
+    var screenWidth = screen.width;
+
+    var banner =  document.getElementById('banner_carousel');
+    //console.log(banner);
+
+    if(screen.width>=1440){
+        banner.src = "img/banner_drogstore_large_1440.png";
+        console.log(banner.src);
+    }if(screen.width>=1024  && screen.width<1440){
+        banner.src = "img/banner_drogstore_large_1024.png";
+        console.log(banner.src);
+        console.log("1024");
+    }if(screen.width>=768  && screen.width<1024){
+        banner.src = "img/banner_drogstore_large_768.png";
+        console.log(banner.src);
+        console.log("768");
+    }if(screen.width>=320  && screen.width<768){
+        banner.src = "img/banner_drogstore_large_400.png";
+        console.log(banner.src);
+        console.log("400");
+    }
+    
+    //document.querySelector('.window-size').innerHTML = windowWidth + 'x' + windowHeight;
+    //document.querySelector('.screen-size').innerHTML = screenWidth + 'x' + screenHeight;
+  
+  };
+  sizeOfThings();
+  
+  window.addEventListener('resize', function(){
+      sizeOfThings();
+  });
